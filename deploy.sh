9fd4f60822e8e091e3c8b171ef35606fe5ed255f
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2019-03-13 18:42:27 -0400

    Fix build.gradle

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git add . && git commit -m "$1" && git push && gradle build && docker build -t maayanlab/g2n:$2 -t maayanlab/g2n:latest . && docker push maayanlab/g2n:$2 && docker push maayanlab/g2n:latest
+git add . && git commit -m "$1" && git push && gradle clean build --refresh-dependencies && docker build -t maayanlab/g2n:$2 -t maayanlab/g2n:latest . && docker push maayanlab/g2n:$2 && docker push maayanlab/g2n:latest
